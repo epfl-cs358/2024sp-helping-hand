6120a0d4266d2d1ca2c96f32066c81971673c8ce
gruvw <gruvw.dev@gmail.com>
2024-05-07 10:30:50 +0200

    feat(app): layout improvements + use real http client

--- code/helping_hand/lib/state/http_client.dart
+++ code/helping_hand/lib/state/http_client.dart
@@ -3,6 +3,6 @@ import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:http/http.dart" as http;
 
 final httpClientProvider = Provider<http.Client>(
-  (ref) => FakeHttpClient(),
-  // (ref) => http.Client(),
+  // (ref) => FakeHttpClient(),
+  (ref) => http.Client(),
 );
--- code/helping_hand/lib/view/components/scroll_body.dart
+++ code/helping_hand/lib/view/components/scroll_body.dart
@@ -10,14 +10,23 @@ class ScrollBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Center(
+    const padding = EdgeInsets.only(
+      top: 14,
+      bottom: 26.0,
+    );
+
+    final content = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        child,
+      ],
+    );
+
+    return Center(
+      child: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.only(
-            top: 14,
-            bottom: 26.0,
-          ),
-          child: child,
+          padding: padding,
+          child: content,
         ),
       ),
     );
