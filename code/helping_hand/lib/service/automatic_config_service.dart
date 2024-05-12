915919a8493ff9f93c03b10229904f20ff64f48a
gruvw <gruvw.dev@gmail.com>
2024-05-12 15:04:33 +0200

    feat(app-auto-config): add the automatic config protocol to the app

---
+++ code/helping_hand/lib/service/automatic_config_service.dart
@@ -0,0 +1,61 @@
+import "dart:convert";
+
+import "package:helping_hand/model/config/configuration.dart";
+import "package:helping_hand/model/device.dart";
+import "package:helping_hand/service/network_discovery_service.dart";
+import "package:helping_hand/state/http_client.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:http/http.dart" as http;
+
+class AutomaticConfigService {
+  static const configPictureEndpoint = "capture";
+
+  static const cvServerConfigEndopint = "analyse";
+  static final cvServerUri = Uri.http("192.168.1.4", cvServerConfigEndopint);
+
+  static const timeoutDuration = Duration(seconds: 5);
+
+  static final povider = Provider<AutomaticConfigService>(
+    (ref) => AutomaticConfigService._(
+      client: ref.watch(httpClientProvider),
+    ),
+  );
+
+  AutomaticConfigService._({
+    required this.client,
+  });
+
+  final http.Client client;
+
+  Future<String> _requestPicture(ConfigDevice configDevice) async {
+    final captureUri = Uri.http(
+      configDevice.network.ipAddress,
+      configPictureEndpoint,
+    );
+
+    final response = await client.get(captureUri).timeout(timeoutDuration);
+    if (response.statusCode != NetworkDeviceService.successCode) {
+      throw Exception("Cannot reach config device.");
+    }
+
+    return base64.encode(response.bodyBytes);
+  }
+
+  Future<String> _analyzeConfig(String picture) async {
+    final response =
+        await client.post(cvServerUri, body: picture).timeout(timeoutDuration);
+
+    if (response.statusCode != NetworkDeviceService.successCode) {
+      throw Exception("Cannot reach cv server.");
+    }
+
+    return response.body;
+  }
+
+  Future<RemoteConfiguration> automaticConfig(ConfigDevice configDevice) async {
+    final config = await _requestPicture(configDevice)
+        .then((picture) => _analyzeConfig(picture));
+
+    return RemoteConfiguration(csvConfig: config);
+  }
+}
--- code/helping_hand/lib/state/fake/fake_http_client.dart
+++ code/helping_hand/lib/state/fake/fake_http_client.dart
@@ -1,5 +1,10 @@
 import "package:flutter/foundation.dart";
+import "package:flutter/services.dart";
+import "package:helping_hand/model/config/button.dart";
+import "package:helping_hand/model/config/configuration.dart";
+import "package:helping_hand/model/config/point_2d.dart";
 import "package:helping_hand/model/network.dart";
+import "package:helping_hand/service/automatic_config_service.dart";
 import "package:helping_hand/service/network_discovery_service.dart";
 import "package:helping_hand/service/remote_controller_service.dart";
 import "package:http/http.dart" as http;
@@ -8,21 +13,25 @@ class FakeHttpClient extends http.BaseClient {
   static const _fakeHttpDelay = Duration(seconds: 1);
   static const _fakeMovementDelay = Duration(milliseconds: 500);
 
+  static const _baseFakeIp = "192.168.1.";
+
   static const _fakeRemotesId = [1, 2];
   static const _fakeConfigId = [3];
 
   static final _fakeRemotes = _fakeRemotesId.map(
     (id) => NetworkDevice(
-      ipAddress: "192.168.1.$id",
+      ipAddress: "$_baseFakeIp$id",
       macAddress: "0:1:$id",
     ),
   );
   static final _fakeCams = _fakeConfigId.map(
     (id) => NetworkDevice(
-      ipAddress: "192.168.1.$id",
+      ipAddress: "$_baseFakeIp$id",
       macAddress: "0:2:$id",
     ),
   );
+
+  static const _fakeCvServer = "${_baseFakeIp}4";
 
   _answerWith(String response) => http.StreamedResponse(
         Stream.value(response.codeUnits),
@@ -46,6 +55,16 @@ class FakeHttpClient extends http.BaseClient {
           "${NetworkDeviceService.configPrefix},${cam.macAddress}",
         );
       }
+
+      if (url.contains(cam.ipAddress) &&
+          url.contains(AutomaticConfigService.configPictureEndpoint)) {
+        final fakeImage = await rootBundle.load("assets/default_image.jpg");
+
+        return http.StreamedResponse(
+          Stream.value(fakeImage.buffer.asInt8List()),
+          NetworkDeviceService.successCode,
+        );
+      }
     }
 
     for (final remote in _fakeRemotes) {
@@ -65,6 +84,22 @@ class FakeHttpClient extends http.BaseClient {
       if (url.contains(RemoteControllerService.shortPressEndoint)) {
         return _answerWith("");
       }
+    }
+
+    if (url.contains(_fakeCvServer) &&
+        url.contains(AutomaticConfigService.cvServerConfigEndopint)) {
+      final defaultConfig = RemoteConfiguration.fromButtons([
+        const Button(
+          label: "my test button 1",
+          pos: Point2D(x: 10, y: 15),
+        ),
+        const Button(
+          label: "my test button 2",
+          pos: Point2D(x: 40, y: 50),
+        ),
+      ]);
+
+      return _answerWith(defaultConfig.csvConfig);
     }
 
     throw Exception("Cannot reach device.");
