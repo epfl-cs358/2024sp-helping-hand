d83c4137e3d8dcdb54a7923b2fdf6114871e0be3
gruvw <gruvw.dev@gmail.com>
2024-05-17 20:56:44 +0200

    feat(server): extract constants to parameters file

--- code/CVserver/coordinates.py
+++ code/CVserver/coordinates.py
@@ -1,30 +1,6 @@
 import cv2
 import numpy as np
-
-# filtering points outside of remote area
-# measured in steps in the plotter's coordinates system
-REMOTE_AREA_ORIGIN = (100, 0)  # top left
-REMOTE_AREA_LIMIT = (500, 850)  # bottom right
-
-# perspective correction
-
-# carmera corners measured in pixels in the camera's coordinates system
-# from the fake remote markers
-MEASURED_CORNERS = (
-    (477, 410),  # top left
-    (1020, 606),  # top right
-    (714, 1354),  # bottom right
-    (230, 1175),  # bottom left
-)
-
-# remote plotter corners measured in steps in the plotter's coordinates system
-# from the fake remote markers
-ACTUAL_CORNERS = (
-    (110, 10),  # top left
-    (490, 10),  # top right
-    (490, 840),  # bottom right
-    (110, 840),  # bottom left
-)
+from parameters import *
 
 
 perspective_matrix = cv2.getPerspectiveTransform(
