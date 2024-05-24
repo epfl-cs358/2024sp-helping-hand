9e1b3ae03eff362c01995e96d7412795eb1f5ad0
Nour Guermazi <100519506+nourguermazi@users.noreply.github.com>
2024-05-24 13:42:33 +0200

    latest changes

--- code/CVserver/edge_detection.py
+++ code/CVserver/edge_detection.py
@@ -11,33 +11,35 @@ def process_image(image_bytes):
     # Decode the numpy array into an image
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
+
     # rotate the image
     img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+
 
     if img is None:
         raise ValueError("Could not decode image")
 
-    y_min = 0
-    y_max = MEASURED_CORNERS[2][0]
-
+    y_min = 200
+    y_max = 1000
+        #MEASURED_CORNERS)[2][0]
     # Crop the image based on y-axis thresholds
     cropped_image = img[:, y_min:y_max]
-    plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
-    plt.show()
+
+
 
     gray = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
 
     # Reduce noise and improve edge detection accuracy
     blurred = cv2.GaussianBlur(gray, (5, 5), 1)
-    # plt.imshow(cv2.cvtColor(blurred, cv2.COLOR_GRAY2BGR))
-    # plt.show()
+    plt.imshow(cv2.cvtColor(blurred, cv2.COLOR_GRAY2BGR))
+    plt.show()
 
-    edges = cv2.Canny(blurred, threshold1=30, threshold2=100)
+    edges = cv2.Canny(blurred, threshold1=70, threshold2=130)
 
-    # plt.imshow(edges)
-    # plt.show()
-    morph = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, np.ones((10,1),np.uint8), iterations=1)
-    morph = cv2.morphologyEx(morph, cv2.MORPH_CLOSE, np.ones((1,10),np.uint8), iterations=1)
+    plt.imshow(edges)
+    plt.show()
+    morph = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, np.ones((20,1),np.uint8), iterations=1)
+    morph = cv2.morphologyEx(morph, cv2.MORPH_CLOSE, np.ones((1,20),np.uint8), iterations=1)
 
     # Find contours
     contours, _ = cv2.findContours(morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -56,27 +58,40 @@ def process_image(image_bytes):
 
     # Fill the desired contour on the mask
     cv2.drawContours(mask, [contour], -1, 255, thickness=cv2.FILLED)
-    eroded_mask = cv2.erode(mask, np.ones((30,30), np.uint8), iterations=1)  # Adjust iterations to control the amount of shrinking
-
+    plt.imshow(mask)
+    plt.show()
+    eroded_mask = cv2.erode(mask, np.ones((40,40), np.uint8), iterations=1)  # Adjust iterations to control the amount of shrinking
+    plt.imshow(eroded_mask)
+    plt.show()
     # Apply the mask to the cropped image
     masked_image = cv2.bitwise_and(cropped_image, cropped_image, mask=eroded_mask)
-    # plt.imshow(masked_image)
-    # plt.show()
+    plt.imshow(masked_image)
+    plt.show()
 
-    edges = cv2.Canny(masked_image, threshold1=70, threshold2=140)
+    edges = cv2.Canny(masked_image, threshold1=70, threshold2=120)
 
-    # plt.imshow(edges)
-    # plt.show()
+    plt.imshow(edges)
+    plt.show()
+
+    kernel_size = 5
+    circular_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size, kernel_size))
+
+    # Apply morphological operation using the circular kernel
+    morph = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, circular_kernel, iterations=1)
 
     # Find contours
-    contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # Use cv2.RETR_CCOMP ? cv2.RETR_TREE?
+    contours, _ = cv2.findContours(morph, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # For now, I use a 'minimum area' threshold to filter small contours, but there is a better way
     min_area = 900
 
     contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_area]
-    # print(len(contours))
-
+    print(len(contours))
+    areas = [cv2.contourArea(contour) for contour in contours]
+    max = np.argmax(areas)
+    contours.pop(max)
+    print(len(contours))
     for contour in contours:
         M = cv2.moments(contour)
         if M["m00"] != 0:
@@ -87,7 +102,7 @@ def process_image(image_bytes):
             buttons.append(["Button", cX, cY])  # Check if it is appropriate to label each coutour a button?
             # This is to draw the contours and the center points (I do it on the cropped image for now)
 
-            cv2.drawContours(img, [contour], -1, (255, 0, 0), 2)
+            cv2.drawContours(img, [contour], -1, (0, 255, 0), 2)
 
             cv2.circle(img, (cX, cY), 5, (0, 0, 255), -1)
 
