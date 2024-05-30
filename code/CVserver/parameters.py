8388d8b44e6fcfa9a27e588518149d527c40a8ad
Jonas Sulzer <jonas@violoncello.ch>
2024-05-30 16:34:48 +0200

    👌 IMPROVE: latest calibration as of 2024.05.30 16:30
    Signed-off-by: Jonas Sulzer <jonas@violoncello.ch>

--- code/CVserver/parameters.py
+++ code/CVserver/parameters.py
@@ -4,27 +4,27 @@
 
 # filtering points outside of remote area
 # measured in steps in the plotter's coordinates system
-REMOTE_AREA_ORIGIN = (20, 170)  # top left
-REMOTE_AREA_LIMIT = (360, 1020)  # bottom right
+REMOTE_AREA_ORIGIN = (22, 123)  # top left
+REMOTE_AREA_LIMIT = (374, 953)  # bottom right
 
 # perspective correction
 
-# carmera corners measured in pixels in the camera's coordinates system
+# camera corners measured in pixels in the camera's coordinates system
 # from the fake remote markers
 MEASURED_CORNERS = (
-    (372, 248),  # top left
-    (806, 246),  # top right
-    (814, 1354),  # bottom right
-    (378, 1358),  # bottom left
+    (345, 96),  # top left
+    (832, 90),  # top right
+    (849, 1344),  # bottom right
+    (352, 1349),  # bottom left
 )
 
 # remote plotter corners measured in steps in the plotter's coordinates system
 # from the fake remote markers
 ACTUAL_CORNERS = (
-    (35, 189),  # top left
-    (345, 188),  # top right
-    (344, 1010),  # bottom right
-    (33, 1011),  # bottom left
+    (42, 143),  # top left
+    (354, 140),  # top right
+    (354, 933),  # bottom right
+    (44, 933),  # bottom left
 )
 
 # === Edge detection parameters ===
